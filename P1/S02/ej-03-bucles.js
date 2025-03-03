eee46d5751238d800a335983ffb1525f9e275bd9
ya.moreno.2021 <ya.moreno.2021@alumnos.urjc.es>
2025-03-03 01:28:44 +0100

    Apuntes

---
+++ P1/S02/ej-03-bucles.js
@@ -0,0 +1,9 @@
+//-- Ejemplo de bucles
+
+//-- Definiendo una constante: Número de mensajes
+const N = 10; 
+
+//-- Bucle para imprimir N mensajes
+for (let i = 0; i < N; i++) {
+    console.log("Mensaje " + i);
+}
